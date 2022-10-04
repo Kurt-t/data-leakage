d730cb905ae48cf8992027af22755f47a2b91618
Kurt <zffu@connect.hku.hk>
2022-10-04 00:45:21 -0400

    finished auto_fix for the toy sample

--- pattern/auto_fix.py
+++ pattern/auto_fix.py
@@ -1,4 +1,6 @@
 import ast
+import astunparse
+import copy
 
 # TODO: how to identify the original dataset?
 # TODO: how to get all dependent lines of split?
@@ -8,8 +10,8 @@ r = open('sample.py', 'r')
 tree = ast.parse(r.read(), 'sample.py')
 
 # tree.body is a list of ast objects: import, assign, expr, etc.
-for line in tree.body:
-    print(ast.dump(line))
+# for line in tree.body:
+#     print(ast.dump(line))
 
 # return if a node is a split
 def is_split(node):
@@ -58,7 +60,7 @@ for node in tree.body:
 # modify the tree:
 train_name = tree.body[index_record['split'][0]].targets[0].id  # TODO: how to make this more intelligent?
 test_name = tree.body[index_record['split'][1]].targets[0].id
-print(train_name, test_name)
+# print(train_name, test_name)
 # first, insert the split node right before the first transform node (or first suspicious transform)
 transform_start = min(index_record['transform'])
 # cannot insert a list of nodes
@@ -77,15 +79,44 @@ for i in index_record['split']:
 # now update and duplicate transforms on train and test dataset
 new_transform_loc = [i + len(index_record['split']) for i in index_record['transform']]
 transform_nodes = [tree.body[i] for i in new_transform_loc]
-print(ast.dump(transform_nodes[0]))
+# print(ast.dump(transform_nodes[0]))
 
-for node in transform_nodes:
-    # TODO: create a new node that replaces all dataset variable to train dataset variable
-    None
 
-# Using NodeTransformer seems not suitable in our problem:
-# class FixLeakage(ast.NodeTransformer):
-#     def visit(self, node):
-#         return None
+# TODO: create a new node that replaces all dataset variable to train dataset variable
+class VariableReplacement(ast.NodeTransformer):
+    def __init__(self, old_var, new_var):
+        self.old_var = old_var
+        self.new_var = new_var
+    
+    def visit(self, node):
+        self.generic_visit(node)  # recursive, post-order
+        if isinstance(node, ast.Name) and node.id == self.old_var:
+            return ast.Name(self.new_var, node.ctx)
+        return node
 
-# ast.NodeTransformer().visit(tree)
+class ModifyTransform(ast.NodeTransformer):
+    def __init__(self, dataset, train_name, test_name):
+        self.dataset = dataset
+        self.train_name = train_name
+        self.test_name = test_name
+    
+    def visit_Expr(self, node):
+        if is_suspicious_transform(node, self.dataset):
+            # replace this node with two nodes
+            train_node = copy.deepcopy(node)
+            test_node = copy.deepcopy(node)
+            # TODO: if the node is df[].call(...df...) type
+            train_node.value.func.value.value.id = self.train_name
+            test_node.value.func.value.value.id = self.test_name
+            variable_replacer = VariableReplacement(self.dataset, self.train_name)
+            train_node = variable_replacer.visit(train_node)
+            test_node = variable_replacer.visit(test_node)
+            
+            return [train_node, test_node]  # ast.copy_location()
+        return node
+
+tree = ModifyTransform(dataset, train_name, test_name).visit(tree)
+# unparse and output
+print('\n', astunparse.unparse(tree))
+
+# print('\n', astunparse.dump(tree))
